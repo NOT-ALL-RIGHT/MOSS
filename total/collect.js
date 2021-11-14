75103a034fa57d8f907c0595b4db6f6b2536b871
Solitaryyy <90620539+Solitaryyy@users.noreply.github.com>
2021-11-14 22:31:37 +0800

    Add files via upload

--- total/collect.js
+++ total/collect.js
@@ -2,17 +2,38 @@ const app = getApp()
 
 Page({
 	data: {
-
+		active: 1,
+		switchTitle1: '名称',
+    	switchTitle2: '时间',
+   	 	itemTitle: '排序方式',
+    	option1: [
+      		{ text: '收藏夹1', value: 0 },
+     	 	{ text: '收藏夹2', value: 1 },
+      		{ text: '收藏夹3', value: 2 },
+    	],
+    	value1: 0,
 	},
 
 	onShow() {
 		this.getTabBar().init();
 	},
-	onClickLeft() {
-		wx.showToast({ title: '点击返回', icon: 'none' });
-	},
 	
-	onClickRight() {
-		wx.showToast({ title: '点击按钮', icon: 'none' });
+	onChange(event) {
+		wx.showToast({
+		  title: `切换到 ${event.detail.name}`,
+		  icon: 'none',
+		});
+	  },
+	
+	onConfirm() {
+		this.selectComponent('#item').toggle();
+	  },
+	
+	onSwitch1Change({ detail }) {
+		this.setData({ switch1: detail });
+	  },
+	
+	onSwitch2Change({ detail }) {
+		this.setData({ switch2: detail });
 	},
 })
