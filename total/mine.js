c5f470a0e8d36908c591a99dfd70fec6d6b75d69
Solitaryyy <90620539+Solitaryyy@users.noreply.github.com>
2021-11-18 11:51:31 +0800

    Add files via upload

--- total/mine.js
+++ total/mine.js
@@ -4,8 +4,8 @@ Page({
   data: {
 	showShare: false,
     options: [
-      { name: '微信', icon: 'wechat', openType: 'share' },
-      { name: '微博', icon: 'weibo' },
+	  { name: '微信', icon: 'wechat', openType: 'share' },
+	  { name: 'QQ', icon: 'qq', openType: 'share' },
       { name: '复制链接', icon: 'link' },
       { name: '分享海报', icon: 'poster' },
       { name: '二维码', icon: 'qrcode' },
